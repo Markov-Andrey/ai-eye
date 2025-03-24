ed01eb97a3b02e5e8ff218c7c6eab7c55753a936
Andre Markov <andremarkov@icloud.com>
2025-03-24 17:55:44 +0300

    add qr decoder

--- decoder.py
+++ decoder.py
@@ -1,16 +1,9 @@
 import os
 import time
+from concurrent.futures import ThreadPoolExecutor
 from pylibdmtx import pylibdmtx
 from pyzbar import pyzbar
 from PIL import Image, ImageDraw
-
-
-def decode_matrix(img_path):
-    import cv2
-    image = cv2.imread(img_path)
-    decoded_objects = pylibdmtx.decode(image)
-
-    return [obj.data.decode('utf-8') for obj in decoded_objects] if decoded_objects else []
 
 
 def decode_qr():
@@ -43,3 +36,33 @@ def decode_qr():
     with open('processed/results1.txt', 'w') as f:
         for qr_code in unique_qrcodes:
             f.write(qr_code + '\n')
+
+
+def find_matrix(img_path):
+    import cv2
+    image = cv2.imread(img_path)
+    decoded_objects = pylibdmtx.decode(image)
+
+    return [obj.data.decode('utf-8') for obj in decoded_objects] if decoded_objects else []
+
+
+def decode_matrix():
+    detected_dir = "detected"
+    all_unique_values = set()
+
+    image_paths = [os.path.join(detected_dir, f) for f in os.listdir(detected_dir) if
+                   f.endswith(('.png', '.jpg', '.jpeg'))]
+
+    image_paths = sorted(image_paths, key=lambda x: int(os.path.splitext(os.path.basename(x))[0].split('_')[1]))
+
+    with ThreadPoolExecutor() as executor:
+        decoded_results = list(executor.map(find_matrix, image_paths))
+
+    all_unique_values.update([data[0] for data in decoded_results if data])
+
+    if all_unique_values:
+        os.makedirs("processed", exist_ok=True)
+        with open("processed/results2.txt", "w") as file:
+            file.write('\n'.join(sorted(all_unique_values)))
+
+    return all_unique_values
--- main.py
+++ main.py
@@ -1,31 +1,8 @@
 import time
 import os
-from concurrent.futures import ThreadPoolExecutor
 from utils import clear_or_create, timer
 from image_processing import process_image
 from decoder import decode_matrix, decode_qr
-
-
-def process_images_from_detected():
-    detected_dir = "detected"
-    all_unique_values = set()
-
-    image_paths = [os.path.join(detected_dir, f) for f in os.listdir(detected_dir) if
-                   f.endswith(('.png', '.jpg', '.jpeg'))]
-
-    image_paths = sorted(image_paths, key=lambda x: int(os.path.splitext(os.path.basename(x))[0].split('_')[1]))
-
-    with ThreadPoolExecutor() as executor:
-        decoded_results = list(executor.map(decode_matrix, image_paths))
-
-    all_unique_values.update([data[0] for data in decoded_results if data])
-
-    if all_unique_values:
-        os.makedirs("processed", exist_ok=True)
-        with open("processed/results2.txt", "w") as file:
-            file.write('\n'.join(sorted(all_unique_values)))
-
-    return all_unique_values
 
 
 if __name__ == "__main__":
@@ -38,6 +15,6 @@ if __name__ == "__main__":
         image_path = os.path.join(image_directory, image_file)
         process_image(image_path)
 
-    process_images_from_detected()
+    decode_matrix()
     decode_qr()
     timer(start_time)
